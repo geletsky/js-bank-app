2977b7a59eb5cebc6d52af7662c76774dce413fb
Geletsky Dev <nsurcov98@gmail.com>
2024-09-09 17:26:43 +0300

    Development of the Render service

--- src/components/screens/home/home.component.js
+++ src/components/screens/home/home.component.js
@@ -1,5 +1,16 @@
-export class Home {
+import { BaseScreen } from '@/core/components/base-screen.component'
+import renderService from '@/core/services/render.service'
+
+import template from './home.template.html'
+import * as styles from './home.module.scss'
+
+export class Home extends BaseScreen {
+	constructor() {
+		super({ title: 'Home' })
+	}
 	render() {
-		return '<p>Home</p>'
+		const element = renderService.htmlToElement(template, [], styles)
+
+		return element.outerHTML
 	}
 }
--- src/components/screens/not-found/not-found.component.js
+++ src/components/screens/not-found/not-found.component.js
@@ -1,4 +1,9 @@
-export class NotFound {
+import { BaseScreen } from '@/core/components/base-screen.component'
+
+export class NotFound extends BaseScreen {
+	constructor() {
+		super({ title: 'Not Found' })
+	}
 	render() {
 		return '<p>Not Found</p>'
 	}
