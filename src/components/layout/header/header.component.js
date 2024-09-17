17726a26db7e6a88c0f2af52ddc906c380e0aa4a
Geletsky Dev <nsurcov98@gmail.com>
2024-09-17 16:08:19 +0300

    Added header component

--- src/components/layout/header/header.component.js
+++ src/components/layout/header/header.component.js
@@ -1,13 +1,31 @@
-import ChildComponent from '@/core/component/child.component';
-import renderService  from '@/core/services/render.service';
+import ChildComponent from '@/core/component/child.component'
+import renderService from '@/core/services/render.service'
 
-import * as styles from './header.module.scss';
-import template from './header.template.html';
+import * as styles from './header.module.scss'
+import template from './header.template.html'
+
+import { Search } from './search/search.component'
+import { UserItem } from '@/components/ui/user-item/user-item.component'
 
 export class Header extends ChildComponent {
-  render() {
-    this.element = renderService.htmlToElement(template, [], styles);
+	constructor() {
+		super()
+	}
 
-    return this.element;
-  }
+	render() {
+		this.element = renderService.htmlToElement(
+			template,
+			[
+				Search,
+				new UserItem({
+					avatarPath:
+						'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+					name: 'Nikita Arnautovich'
+				})
+			],
+			styles
+		)
+
+		return this.element
+	}
 }
