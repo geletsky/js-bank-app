6e1a83feea52dc85252d5fa5e1f980512ea39e92
Geletsky Dev <nsurcov98@gmail.com>
2024-10-03 16:54:29 +0300

    Fix bug in query.lib

--- src/core/query/query.lib.js
+++ src/core/query/query.lib.js
@@ -69,6 +69,9 @@ export async function query({
 	} catch (errorData) {
 		const errorMessage = extractErrorMessage(errorData)
 
+		if (onError) {
+			onError(errorMessage)
+		}
 
 		new NotificationService().show('error', errorMessage)
 	} finally {
